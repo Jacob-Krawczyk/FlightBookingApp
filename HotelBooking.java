c651e2835c451c89705d646406e8898fc9353224
SEN <Senz>
2022-03-15 14:28:53 +0800

    HotelBooking

---
+++ HotelBooking.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class HotelBooking {
+	
+	protected ArrayList<Profile> travelers;
+	private ArrayList<Room> roomes;
+	private Hotel hotel;
+	
+	public void book()
+	{
+		
+	}
+	public void cancel()
+	{
+		
+	}
+	
+}
