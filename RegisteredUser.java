1259390103c3a0a52cae199acaa2c7442bed3be5
Jackie Dinh <kdinh@email.sc.edu>
2022-03-15 13:58:28 -0400

    Added RegisteredUser Class

---
+++ RegisteredUser.java
@@ -0,0 +1,34 @@
+public class RegisteredUser {
+    private Profile userProfile;
+    private ArrayList<FlightBooking> flightBookings;
+    private ArrayList<HotelBooking> hotelBookings;
+    private ArrayList<Profile> friendList;
+
+    public RegisteredUser(Profile user) {
+        user = userProfile;
+    }
+    public void setPreferences() {
+
+    }
+    public void accessTicketInfo() {
+
+    }
+    private boolean ofAge() {
+
+    }
+    public void addFlyer(ArrayList<Profile>) {
+
+    }
+    public void addNonUserFriend(Profile friend) {
+
+    }
+    public void addUserFriend() {
+
+    }
+    public void removeFriend() {
+
+    }
+    public void accessItinerary(ArrayList<FlightBooking> flightBookings, ArrayList<HotelBooking> hotelBookings) {
+
+    }
+}
