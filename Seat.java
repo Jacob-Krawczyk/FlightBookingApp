b364c34927ad79958a813f622708f7598ccdc441
SEN <Senz>
2022-03-15 14:05:10 +0800

    Seat

---
+++ Seat.java
@@ -0,0 +1,12 @@
+
+public class Seat {
+	private boolean occupied;
+	private String seatNumber;
+	
+	public Seat(boolean occupied, String seatNumber)
+	{
+		this.occupied=occupied;
+		this.seatNumber=seatNumber;
+	}
+
+}
