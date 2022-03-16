2639d580765f974816577c40d95262f0a17b1935
Jacob Krawczyk <krawczj@email.sc.edu>
2022-03-16 15:11:24 -0400

    updated flight.json

--- Flight.java
+++ Flight.java
@@ -5,8 +5,6 @@
 import java.util.ArrayList;
 
  public class Flight {
-    private ArrayList <String> list_ofregisteredPlane;
-    private int numofpassengers;
     private String departLocation;
     private String arrivalLocation;
     private int numofTicketsAvailable;
