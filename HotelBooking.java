0a6fbd2d7046e69797d41752f82827de570d43ae
sionaravi <arthiravi2524@gmail.com>
2022-03-17 13:36:36 -0400

    Hotel Booking

---
+++ HotelBooking.java
@@ -0,0 +1,18 @@
+/**
+ * Hotel Booking
+ * @author JavaFine
+ */
+import java.util.ArrayList;
+
+public class HotelBooking {
+    protected ArrayList<Profile> travelers;
+    private ArrayList<Room> rooms;
+    private Flight flight;
+    
+    public void book() {
+        
+    }
+    public void cancel() {
+
+    }
+}
