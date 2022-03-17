7876dd5d9665d03f8249c1fd42e108755e6104ce
Jacob Krawczyk <krawczj@email.sc.edu>
2022-03-17 13:38:29 -0400

    Created Seat.java

---
+++ Seat.java
@@ -0,0 +1,14 @@
+/**
+ * Flight Application
+ * @author JavaFine
+ */
+
+ public class Seat {
+    private bool occupied;
+    private String seatNumber;
+
+    public seat(bool occupied, String seatNumber) [
+        occupied = this.occupied;
+        seatNumber = this.seatNumber;
+    ]
+ } 
