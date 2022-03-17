009864c7cbca54c171703bc440535128a3c5e2d2
sionaravi <arthiravi2524@gmail.com>
2022-03-17 13:49:45 -0400

    added amenities

---
+++ Amenities.java
@@ -0,0 +1,15 @@
+/**
+ * List of Amenities
+ * @author JavaFine
+ */
+
+public enum Amenities{
+    POOL,
+    GYM,
+    TOILETRIES,
+    COFFEEKIT,
+    WIFI,
+    PARKING,
+    SMOKE,
+    MEAL
+}
