aab60c263d430785739d477bc20aa3fa1259abcf
sionaravi <arthiravi2524@gmail.com>
2022-03-18 15:28:01 -0400

    added hotel.java

---
+++ Hotel.java
@@ -0,0 +1,46 @@
+/**
+ * Hotel Class
+ * @author JavaFine
+ */
+public class Hotel{
+    private String name;
+    private String location;
+    private ArrayList<Room> rooms;
+
+    private ArrayList<Amenities> amenities;
+    private ArrayList<Accessibility> accessibility;
+
+    public Hotel (String name, String checkInDay, String checkOutDay, String location, int numberOfBeds, String roomType){
+        this.name = name;
+        this.checkInDay = checkInDay;
+        this.checkOutDay = checkOutDay;
+        this.location = location;
+        this.numberOfBeds = numberOfBeds;
+        this.roomType = roomType;
+    }
+    public void addAmenities{
+
+    }
+    public void removeAmenities{
+
+    }
+    public void addAccessibility{
+
+    }
+    public void removeAccessibility{
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
