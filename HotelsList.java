b1bbdc3c79d8ef8c9987655b6db2afee2472e8f6
xingcheng ren <72652630+1146984342@users.noreply.github.com>
2022-03-30 02:26:32 +0800

    Add files via upload
    add remove and cancel method for hotel, flight and users lists

--- HotelsList.java
+++ HotelsList.java
@@ -1,217 +1,206 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.UUID;
 
 /**
  * A Singleton Listing of Hotels
+ * 
  * @author JavaFine
  */
 public class HotelsList {
-    private HotelDatabaseLoader hotelLoader;
-    private ArrayList<Hotel> hotels;
-    private ArrayList<Room> rooms;
-    private ArrayList<Hotel> returnList;
-    private static HotelsList hotelList;
-    protected ArrayList<Profile> travelers;
+  private HotelDatabaseLoader hotelLoader;
+  private ArrayList<Hotel> hotels;
+  private ArrayList<Hotel> returnList;
+  private static HotelsList hotelList;
 
-    /**
-     * Creates a single instance of HotelsList and returns that single instance
-     * @return HotelsList
-     */
-    public static HotelsList getInstance() {
-        if(hotelList == null) {
-            hotelList = new HotelsList();
+  /**
+   * Creates a single instance of HotelsList and returns that single instance
+   * 
+   * @return HotelsList
+   */
+  public static HotelsList getInstance() {
+    if (hotelList == null) {
+      hotelList = new HotelsList();
+    }
+    return hotelList;
+  }
+
+  /**
+   * Clears search array list
+   */
+  private void clearSearch() {
+    returnList.clear();
+  }
+
+  /**
+   * Returns list of all hotels from database
+   * 
+   * @return list of all hotels from database
+   */
+  public ArrayList<Hotel> getAllHotels() {
+    hotels = HotelDatabaseLoader.getHotel();
+    return hotels;
+  }
+
+  /**
+   * Returns hotel based on UUID
+   * 
+   * @param id
+   * @return hotel
+   */
+  public Hotel getHotelByUUID(UUID id) {
+    for (Hotel hotel : hotels) {
+      if (hotel.getID().equals(id))
+        return hotel;
+    }
+    return null;
+  }
+
+  /**
+   * Returns hotel search result
+   * 
+   * @param location
+   * @param amenities
+   * @param accessibility
+   * @param roomType
+   * @param numOfBeds
+   * @return hotel search array list
+   */
+  public ArrayList<Hotel> getSearch(String location, ArrayList<Amenities> amenities,
+      ArrayList<Accessibility> accessibility, String roomType, int numOfBeds) {
+    clearSearch();
+    getHotelByLocation(location);
+    getHotelByAmenities(amenities);
+    getHotelByAccessibility(accessibility);
+    getHotelByRoomType(roomType);
+    getHotelByNumberOfBeds(numOfBeds);
+    return returnList;
+  }
+
+  /**
+   * Adds hotels that match location to return list
+   * 
+   * @param location
+   */
+  private void getHotelByLocation(String location) {
+    for (Hotel hotel : hotels) {
+      String hotelLocation = hotel.getLocation();
+      if (hotelLocation.equals(location)) {
+        returnList.add(hotel);
+      }
+    }
+  }
+
+  /**
+   * Removes hotels that don't match amentiies
+   * 
+   * @param amenities
+   */
+  private void getHotelByAmenities(ArrayList<Amenities> amenities) {
+    EnumSet<Amenities> amenitiesList = EnumSet.allOf(Amenities.class);
+    ArrayList<Amenities> removeList = new ArrayList<Amenities>();
+    for (Amenities amen : amenitiesList) {
+      removeList.add(amen);
+    }
+    removeList.removeAll(amenities);
+    for (Hotel search : returnList) {
+      ArrayList<Amenities> amenity = search.getAmenities();
+      for (Amenities rem : removeList) {
+        for (Amenities cur : amenity) {
+          if (cur.equals(rem)) {
+            returnList.remove(search);
+          }
         }
-        return hotelList;
+      }
     }
+  }
 
-    /**
-     * Clears search array list
-     */
-    private void clearSearch() {
-        returnList.clear();
+  /**
+   * Removes hotels that don't match accessibility
+   * 
+   * @param accessibility
+   */
+  private void getHotelByAccessibility(ArrayList<Accessibility> accessibility) {
+    EnumSet<Accessibility> accessibilityList = EnumSet.allOf(Accessibility.class);
+    ArrayList<Accessibility> removeList = new ArrayList<Accessibility>();
+    for (Accessibility access : accessibilityList) {
+      removeList.add(access);
     }
-
-    /**
-     * Returns list of all hotels from database
-     * @return list of all hotels from database
-     */
-    public ArrayList<Hotel> getAllHotels() {
-        hotels = HotelDatabaseLoader.getHotel();
-        return hotels;
-    }
-
-    /**
-     * Returns hotel based on UUID
-     * @param id
-     * @return hotel
-     */
-    public Hotel getHotelByUUID(UUID id) {
-        for (Hotel hotel: hotels) {
-            if (hotel.getID().equals(id))
-                return hotel;
+    removeList.removeAll(accessibility);
+    for (Hotel search : returnList) {
+      for (Accessibility rem : removeList) {
+        ArrayList<Accessibility> accessibilities = search.getAccessibility();
+        for (Accessibility cur : accessibilities) {
+          if (cur.equals(rem)) {
+            returnList.remove(search);
+          }
         }
-        return null;
+      }
     }
+  }
 
-    /**
-     * Returns hotel search result 
-     * @param location
-     * @param amenities
-     * @param accessibility
-     * @param roomType
-     * @param numOfBeds
-     * @return hotel search array list
-     */
-    public ArrayList<Hotel> getSearch(String location, ArrayList<Amenities> amenities, ArrayList<Accessibility> accessibility, String roomType, int numOfBeds, Double rating) {
-        clearSearch();
-        getHotelByLocation(location);
-        getHotelByAmenities(amenities);
-        getHotelByAccessibility(accessibility);
-        getHotelByRoomType(roomType);
-        getHotelByNumberOfBeds(numOfBeds);
-        getHotelByRatings(rating);
-        return returnList;
+  /**
+   * Removes hotels that don't match room type
+   * 
+   * @param roomType
+   */
+  public void getHotelByRoomType(String roomType) {
+    if (!roomType.equals("none")) {
+      for (Hotel search : returnList) {
+        if (!search.getRoomType().equals(roomType)) {
+          returnList.remove(search);
+        }
+      }
     }
+  }
 
-    public ArrayList<Hotel> getFourMatchesByLocation(String location) {
-        clearSearch();
-        getHotelByLocation(location);
-        for (int i = 4; i < returnList.size(); i++) {
-            returnList.remove(returnList.get(i));
+  /**
+   * Removes hotels that don't match number of beds
+   * 
+   * @param numOfBeds
+   */
+  public void getHotelByNumberOfBeds(int numOfBeds) {
+    if (numOfBeds != 0) {
+      for (Hotel search : returnList) {
+        if (search.getnumOfBeds() == numOfBeds) {
+          returnList.add(search);
         }
-        return returnList;
-    }
-    private void getHotelByRatings(double ratings) {
-        for(Hotel hotel: hotels) {
-            double hotelRating = hotel.getRating();
-            if(hotelRating != ratings) {
-                returnList.remove(hotel);
-            }
-        }
-    }
 
-    /**
-     * Adds hotels that match location to return list
-     * @param location
-     */
-    private void getHotelByLocation(String location) {
-        for(Hotel hotel:hotels) {
-            String hotelLocation = hotel.getLocation();
-            if(hotelLocation.equals(location)) {
-                returnList.add(hotel);
-            }
-        }
+      }
     }
+  }
 
-    /**
-     * Removes hotels that don't match amentiies
-     * @param amenities
-     */
-    private void getHotelByAmenities(ArrayList<Amenities> amenities) {
-        EnumSet<Amenities> amenitiesList = EnumSet.allOf(Amenities.class);
-        ArrayList<Amenities> removeList = new ArrayList<Amenities>();
-        for(Amenities amen: amenitiesList) {
-            removeList.add(amen);
-        }
-        removeList.removeAll(amenities);
-        for(Hotel search: returnList) {
-            ArrayList<Amenities> amenity = search.getAmenities();
-            for(Amenities rem: removeList) {
-                for(Amenities cur: amenity) {
-                    if(cur.equals(rem)) {
-                        returnList.remove(search);
-                    }
-                }
-            }
-        }
+  public void printRoomByDateAndTime(Hotel hotel, Date checkInDate, String checkinTime,
+      Date checkOutDate, String checkOutTime) {
+    ArrayList<Room> rooms = hotel.getHotelRooms();
+    for (int i = 0; i < rooms.size(); i++) {
+      if (rooms.get(i).getRoomCheckInDate().equals(checkInDate)
+          && rooms.get(i).getRoomCheckInTime().equals(checkinTime)
+          && rooms.get(i).getRoomCheckOutDate().equals(checkOutDate)
+          && rooms.get(i).getRoomCheckOutTime().equals(checkOutTime)) {
+        System.out.println(rooms.get(i).toString());
+      }
     }
+  }
 
-    /**
-     * Removes hotels that don't match accessibility
-     * @param accessibility
-     */
-    private void getHotelByAccessibility(ArrayList<Accessibility> accessibility) {
-        EnumSet<Accessibility> accessibilityList = EnumSet.allOf(Accessibility.class);
-        ArrayList<Accessibility> removeList = new ArrayList<Accessibility>();
-        for(Accessibility access: accessibilityList) {
-            removeList.add(access);
-        }
-        removeList.removeAll(accessibility);
-        for(Hotel search: returnList) {
-            for(Accessibility rem: removeList) {
-                ArrayList<Accessibility> accessibilities = search.getAccessibility();
-                for(Accessibility cur: accessibilities) {
-                    if(cur.equals(rem)) {
-                        returnList.remove(search);
-                    }
-                }
-            }
-        }
-    }
-    
-    /**
-     * Removes hotels that don't match room type
-     * @param roomType
-     */
-    public void getHotelByRoomType(String roomType) {
-        if(!roomType.equals("none")) {
-            for(Hotel search: returnList) {
-                if(!search.getRoomType().equals(roomType)) {
-                    returnList.remove(search);
-                }
-            }
-        }
-    }
+  public Room getRoom(Hotel hotel, int roomNum) {
+    ArrayList<Room> rooms = hotel.getHotelRooms();
+    return rooms.get(roomNum);
+  }
 
-    /**
-     * Removes hotels that don't match number of beds
-     * @param numOfBeds
-     */
-    public void getHotelByNumberOfBeds(int numOfBeds) {
-        if(numOfBeds != 0) {
-            for(Hotel search: returnList) {
-                if(search.getnumOfBeds() == numOfBeds) {
-                    returnList.add(search);
-                }
-                
-            }
-        }
+  public void cancelHotel(UUID hotelID) {
+    for (Hotel hotel : hotels) {
+      if (hotel.getID().equals(hotelID)) {
+        int pos = this.hotels.indexOf(hotel);
+        hotels.remove(pos);
+      }
     }
+  }
 
-    /**
-     * Prints rooms that match user's preferred check-in and check-out day and time
-     * @param hotel
-     * @param checkInDate
-     * @param checkinTime
-     * @param checkOutDate
-     * @param checkOutTime
-     */
-    public void printRoomByDateAndTime(Hotel hotel, Date checkInDate, String checkinTime, Date checkOutDate, String checkOutTime) {
-        ArrayList<Room> rooms = hotel.getHotelRooms();
-        for(int i = 0; i < rooms.size(); i++) {
-            if (rooms.get(i).getRoomCheckInDate().equals(checkInDate) && rooms.get(i).getRoomCheckInTime().equals(checkinTime)
-                    && rooms.get(i).getRoomCheckOutDate().equals(checkOutDate)
-                    && rooms.get(i).getRoomCheckOutTime().equals(checkOutTime)) {
-                System.out.println(rooms.get(i).toString());
-            }
-        }
-    }
+  public void bookHotel(Hotel hotel) {
+    hotels.add(hotel);
+  }
 
-    /**
-     * Returns list of rooms hotel has
-     * @param hotel
-     * @param roomNum
-     * @return array list of rooms
-     */
-    public Room getRoom(Hotel hotel, int roomNum) {
-        ArrayList<Room> rooms = hotel.getHotelRooms();
-        return rooms.get(roomNum);
-    }
 
-    public void addRoom(Room room) {
-        rooms.add(room);
-    }
-
-    public void addTraveler(Profile profile) {
-        travelers.add(profile);
-    }
 }
--- UserList.java
+++ UserList.java
@@ -1,167 +1,129 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * A Singleton Listing of Users
+ * 
  * @author JavaFine
  */
 public class UserList {
-    private UserDatabaseLoader userDatabaseLoader;
-    private UserDatabaseWriter userDatabaseWriter;
-    private ArrayList<RegisteredUser> users;
-    private static UserList userList;
+  private UserDatabaseLoader userDatabaseLoader;
+  private UserDatabaseWriter userDatabaseWriter;
+  private ArrayList<RegisteredUser> users;
+  private static UserList userList;
 
-    /**
-     * Creates and returns a single instance of UserList
-     * @return UserList
-     */
-    public static UserList getInstance() {
-        if(userList == null) {
-            userList = new UserList();
-        }
-        return userList;
+  /**
+   * Creates and returns a single instance of UserList
+   * 
+   * @return UserList
+   */
+  public static UserList getInstance() {
+    if (userList == null) {
+      userList = new UserList();
     }
+    return userList;
+  }
 
-    /**
-     * Returns user by UUID
-     * @param id
-     * @return registered user 
-     */
-    public RegisteredUser getUserByUUID(UUID id) {
-        for (RegisteredUser user: users) {
-            if (user.getID().equals(id))
-                return user;
-        }
-        return null;
+  /**
+   * Returns user by UUID
+   * 
+   * @param id
+   * @return registered user
+   */
+  public RegisteredUser getUserByUUID(UUID id) {
+    for (RegisteredUser user : users) {
+      if (user.getID().equals(id))
+        return user;
     }
-    
-    /**
-     * Returns user by username
-     * @param username
-     * @return registered user
-     */
-    public RegisteredUser getUserByUsername(String username) {
-        for(RegisteredUser user: users) {
-            if(user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
-    }
-    /**
-     * Adds user to list and to database files
-     * @param user
-     */
-    public void addUser(RegisteredUser user) {
-        users.add(user);
-        ((List<RegisteredUser>) userDatabaseWriter).add(user);
-    }
+    return null;
+  }
 
-    /**
-     * Removes user from list and from database file
-     * @param user
-     */
-    public void removeUser(RegisteredUser user) {
-        users.remove(user);
-        ((List<RegisteredUser>) userDatabaseWriter).remove(user);
+  /**
+   * Returns user by username
+   * 
+   * @param username
+   * @return registered user
+   */
+  public RegisteredUser getUserByUsername(String username) {
+    for (RegisteredUser user : users) {
+      if (user.getUsername().equals(username)) {
+        return user;
+      }
     }
+    return null;
+  }
 
-    /**
-     * Adds friend to friend list of user
-     * @param friend
-     */
-    public void addFriend(Friend friend) {
-        userDatabaseWriter.addFriend(friend);
+  /**
+   * Adds user to list and to database files
+   * 
+   * @param user
+   */
+  public void addUser(RegisteredUser user) {
+    users.add(user);
+    // ((List<RegisteredUser>) userDatabaseWriter).add(user);
+  }
+
+  /**
+   * Removes user from list and from database file
+   * 
+   * @param user
+   */
+  public void removeUser(UUID userID) {
+    for (RegisteredUser user : users) {
+      if (user.getID().equals(userID)) {
+        int pos = this.users.indexOf(user);
+        users.remove(pos);
+      }
     }
+    // ((List<RegisteredUser>) userDatabaseWriter).remove(user);
+  }
 
-    /**
-     * Removes friend from friend list of user
-     * @param friend
-     */
-    public void removeFriend(Friend friend) {
-        userDatabaseWriter.removeFriend(friend);
+
+  /**
+   * Returns the list of friends associated with current user
+   * 
+   * @return list of friends
+   */
+  public ArrayList<Friend> getFriendsList() {
+    return userList.getFriendsList();
+  }
+
+  /**
+   * Returns user based on inputted username and password
+   * 
+   * @param username
+   * @param password
+   * @return user based on inputted username and password
+   */
+  public RegisteredUser getUser(String username, String password) {
+    for (RegisteredUser user : users) {
+      if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+        return user;
+      }
     }
+    return null;
+  }
 
-    /**
-     * Prints list of friends associated with current user
-     * @param currentUser
-     * @return list of friends
-     */
-    public void getFriendsList(RegisteredUser currentUser) {
-        ArrayList<Friend> friendsList = currentUser.getFriends();
-        for (Friend friend : friendsList) {
-            System.out.println(friend.toString());
-        }
-    }
+  /*
+   * public void cancelFlight(ArrayList<Flight> flights) { for(Flight flight: flights) {
+   * userDatabaseWriter.removeFlight(flight); } }
+   * 
+   * public void bookFlight(ArrayList<Flight> flights) { for(Flight flight: flights) {
+   * userDatabaseWriter.addFlight(flight); } }
+   */
+  /*
+   * public void cancelHotel(Hotel hotel) { userDatabaseWriter.removeHotel(hotel); }
+   * 
+   * public void bookHotel(Hotel hotel) { userDatabaseWriter.addHotel(hotel); }
+   */
 
-    public void addNonUserFriend(RegisteredUser currentUser, String first, String last, String dob, String discount) {
-        Friend friend = currentUser.addNonUserFriend(first, last, dob, discount);
-        addFriend(friend);
-    }
-
-    public void addUserFriend(RegisteredUser currentUser, String username) {
-        Friend friend = currentUser.addUserFriend(username);
-        addFriend(friend);
-    }
-
-    /**
-     * Returns user based on inputted username and password
-     * @param username
-     * @param password
-     * @return user based on inputted username and password
-     */
-    public RegisteredUser getUser(String username, String password) {
-        
-        for(RegisteredUser user: users) {
-            if(user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Removes flight from user's flight list
-     * @param flights
-     */
-    public void cancelFlight(RegisteredUser currentUser, FlightBooking flight) {
-        currentUser.cancelFlight(flight);
-    }
-
-    /**
-     * Adds flight to user's flight list
-     * @param flights
-     */
-    public void bookFlight(RegisteredUser currentUser, FlightBooking flight) {
-        currentUser.bookFlight(flight);
-    }
-
-    /**
-     * Removes hotel from user's hotel list
-     * @param hotel
-     */
-    public void cancelHotel(RegisteredUser currentUser, HotelBooking hotel) {
-        currentUser.cancelHotel(hotel);
-    }
-
-    /**
-     * Adds hotel to user's hotel list
-     * @param hotel
-     */
-    public void bookHotel(RegisteredUser currentUser, HotelBooking hotel) {
-        currentUser.bookHotel(hotel);
-    }
-
-    /**
-     * Returns list of all registered users
-     * @return list of users
-     */
-    public ArrayList<RegisteredUser> getAllUsers() {
-        users = userDatabaseLoader.getUser();
-        return users;
-    }
-
-    public void printItinerary(RegisteredUser currentUser) {
-        currentUser.accessItinerary();
-    }
-
+  /**
+   * Returns list of all registered users
+   * 
+   * @return list of users
+   */
+  public ArrayList<RegisteredUser> getAllUsers() {
+    users = userDatabaseLoader.getUser();
+    return users;
+  }
 }
