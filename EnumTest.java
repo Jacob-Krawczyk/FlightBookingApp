e8116ec6c76803372cc1996ceab98dc57390060e
Jackie Dinh <kdinh@email.sc.edu>
2022-04-05 13:58:34 -0400

    All Enum Tests

---
+++ EnumTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing All Enum ToStrings
+ * @author JavaFine
+ */
+
+public class EnumTest {
+    @Test
+    void testAccessibilityToString() {
+        String accessibility = Accessibility.values().toString();
+        System.out.println(accessibility);
+    }
+
+    @Test 
+    void testAirlineCompanyToString() {
+        String comp = AirlineCompany.values().toString();
+        System.out.println(comp);
+    }
+
+    @Test 
+    void testAmenitiesToString() {
+        String amenities = Amenities.values().toString();
+        System.out.println(amenities);
+    }
+
+    @Test
+    void testFlightClassToString() {
+        String classes = FlightClass.values().toString();
+        System.out.println(classes);
+    }
+
+    @Test
+    void testHotelRoomTypesToString() {
+        String roomType = HotelRoomTypes.values().toString();
+        System.out.println(roomType);
+    }
+}
