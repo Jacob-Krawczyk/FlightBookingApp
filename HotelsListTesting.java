1dd93f3cfc0a2352d9111e5873b288cc443791c4
Jackie Dinh <kdinh@email.sc.edu>
2022-04-09 02:05:34 -0400

    added test method

--- HotelsListTesting.java
+++ HotelsListTesting.java
@@ -15,10 +15,39 @@ public class HotelsListTesting {
     }
 
     @Test
-    void getHotelByUUID() {
+    void getHotelByUUID_ShouldPass() {
         try {
-            Hotel hotel = HotelsList.getInstance().getHotelByUUID(2ead4186-a210-406d-a130-ba2c51b4ff78);
             UUID uuid = new UUID(2ead4186-a210-406d-a130-ba2c51b4ff78);
+            Hotel hotel = HotelsList.getInstance().getHotelByUUID(uuid);
+            String name = "Marriott of Seattle";
+            String city = "Seattle";
+            ArrayList<Room> rooms = new ArrayList<Room>();
+            int numOfBed = 2;
+            String roomType = "Deluxe";
+            SimpleDateFormat format = new SimpleDateFormat("mm/dd/yyyy");
+            Date checkInDay = format.parse("08/12/2022");
+            Date checkOutDay = format.parse("08/14/2022");
+            String checkInTime = "3:30 PM";
+            String checkOuttime = "12:00 PM";
+            String price = "$100";
+            Room room1 = new Room(roomType, numOfBed, checkInDay, checkOutDay, checkInTime, checkOutTime, price);
+            Room room2 = new Room(roomType, numOfBed, checkInDay, checkOutDay, checkInTime, checkOutTime, price);
+            rooms.add(room1);
+            rooms.add(room2);
+            Hotel actual = Hotel(uuid, name, city, rooms);
+            assertEquals(hotel.getHotelName(), actual.getHotelName());
+            assertEquals(hotel.getLocation(), actual.getLocation());
+            assertEquals(hotel.getHotelRooms(), actual.getHotelRooms());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getHotelByUUID_NoMatches() {
+        try {
+            UUID uuid = new UUID(2ead4234-a210-823d-a130-ba2c51b4ff78);
+            Hotel hotel = HotelsList.getInstance().getHotelByUUID(uuid);
             String name = "Marriott of Seattle";
             String city = "Seattle";
             ArrayList<Room> rooms = new ArrayList<Room>();
